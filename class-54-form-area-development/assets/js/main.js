a755218144cb8f3f9418b5a7dac5c675a870a408
parthatalukder <33701251+parthatalukder@users.noreply.github.com>
2017-12-19 01:34:49 +0600

    Add files via upload

---
+++ class-54-form-area-development/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('.datepicker').datepicker();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
